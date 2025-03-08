06258232cb18dccb9cf5910ce5cfc205f4e0a4c6
nn <jaksow05@gmail.com>
2025-03-08 02:29:42 +0100

    chore: fix environments and split to prod/dev env

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  ssr: {noExternal: ["@mui/material", "@mui/utils", "@mui/base", "@mui/icons-material"]},
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    ssr: {noExternal: ["@mui/material", "@mui/utils", "@mui/base", "@mui/icons-material"]},
+    define: {
+      'process.env': env
+    }
+  }
+
   // optimizeDeps: {
   //   include: ['@mui/icons-material'],
   // },
