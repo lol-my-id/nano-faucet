4b5b06023dcb7cd13aa557f3147a216d08dd3ed4
nn <jaksow05@gmail.com>
2025-03-09 23:17:04 +0100

    fix: await 2 promises concurrently instead of sequentially

--- src/contexts/Provider.tsx
+++ src/contexts/Provider.tsx
@@ -2,15 +2,16 @@
 import { atom, useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 import { useEffect } from 'react';
-import { NextRollData, ReferralData } from '../types/api';
+import { NextRollData, ReferralSystemData } from '../types/api';
 import { ADDRESS_STORAGE_NAME, getCurrencyFromAddress, validateNanoAddress } from '../modules/utils';
 import { getNextRollData, getReferralData } from '../modules/api';
+import { cToast } from '../components/Toast';
 
 // Atom to store address from localStorage
 export const addressAtom = atomWithStorage<string | null>(ADDRESS_STORAGE_NAME, null);
 
 // Atom to store Referral data
-export const referralDataAtom = atom<ReferralData | null>(null);
+export const referralDataAtom = atom<ReferralSystemData | null>(null);
 
 // Atom to store next available roll time
 export const nextRollAtom = atom<NextRollData | null>(null);
@@ -26,16 +27,24 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
     const [, setCurrency] = useAtom(currencyAtom);
 
     useEffect(() => {
-        const initializeData = () => {
-            if (address && address.length > 0 && validateNanoAddress(address)) {
-                // Fetch referral data and next roll data, not async for better code readability
-                getReferralData(address).then(setReferralData).catch(()=>{});
-                getNextRollData(address).then(setNextRoll).catch(() => {});
-                setCurrency(getCurrencyFromAddress(address));
-            }
-        };
-
-        initializeData();
+        if (address && validateNanoAddress(address)) {
+            Promise.allSettled([
+                getReferralData(address),
+                getNextRollData(address)
+            ]).then(([referralData, nextRollData]) => {
+                // Handle referral data
+                if (referralData.status === 'fulfilled') {
+                    setReferralData(referralData.value);
+                } // No Toast since that probably means the user is first time user
+        
+                // Handle next roll data
+                console.log("nextRollData", nextRollData);
+                if (nextRollData.status === 'fulfilled') {
+                    setNextRoll(nextRollData.value);
+                } else cToast.error("Failed to fetch next roll data");
+            });
+            setCurrency(getCurrencyFromAddress(address));
+        }
     }, [address, setAddress, setReferralData, setNextRoll, setCurrency]);
 
   return <>{children}</>;
