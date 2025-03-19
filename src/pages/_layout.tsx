19965249846fec2ca4d43952c055defa8b6ffb31
nn <jaksow05@gmail.com>
2025-03-19 17:17:22 +0100

    chore: update site metatags

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -9,10 +9,10 @@ type RootLayoutProps = { children: ReactNode };
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
-    <title>NANOFAUCET</title>
+    <title>{process.env.WAKU_PUBLIC_DOMAIN}</title>
     <link rel="icon" type="image/png" href="/images/favicon.png" />
-    <meta name="title" content="NANOFAUCET - The coolest anonymous NANO Faucet!" />
-    <meta name="description" content="NANOFAUCET is the top anonymous NANO faucet, offering free NANO cryptocurrency with no identity required. Claim your NANO securely and privately today!" />
+    <meta name="title" content={`${process.env.WAKU_PUBLIC_DOMAIN} - The coolest anonymous NANO Faucet!`} />
+    <meta name="description" content={`${process.env.WAKU_PUBLIC_DOMAIN} is the top anonymous NANO faucet, offering free NANO cryptocurrency with no identity required. Claim your NANO securely and privately today!`} />
     <meta name="keywords" content="nano, crypto, faucet, NANO cryptocurrency, Anonymous faucets, Free NANO, Crypto giveaways, Digital currencies, Online transactions, Fast and secure payouts, User privacy, Decentralized finance, Crypto rewards, Blockchain technology, Anonymous transactions, Crypto wallets, Instant transfers, No KYC" />
     <meta name="robots" content="index, follow" />
     <DataProvider>
